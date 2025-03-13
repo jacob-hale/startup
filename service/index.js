e184c9a1554cde8e2fff2eaf7678711922c6428f
Jacob Hale <Jchale23@byui.edu>
2025-03-13 11:36:48 -0600

    index,js start

---
+++ service/index.js
@@ -0,0 +1,9 @@
+import express from 'express';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
