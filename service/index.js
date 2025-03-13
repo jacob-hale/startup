4b86bf401250c21e1372bf420d5b985320e45371
Jacob Hale <Jchale23@byui.edu>
2025-03-13 11:43:07 -0600

    service

--- service/index.js
+++ service/index.js
@@ -7,3 +7,17 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
+
+app.use(express.static(join(__dirname, '../public')));
+
+app.use(express.json());
+
+// Example endpoint
+app.get('/api/mood-entries', (req, res) => {
+  // Mock data for mood entries
+  const entries = [
+    { date: '10/10/2023', mood: '😊 happy', note: 'Feeling great!' },
+    { date: '10/11/2023', mood: '😢 sad', note: 'Feeling down.' },
+  ];
+  res.json(entries);
+});
